38f9033c28cd8ff288c906d88c340e9468db87e3
stagiaire <stagiaire@iMac20.local>
2017-05-05 16:55:30 +0200

    import du projet

--- eval2/ViewController.swift
+++ eval2/ViewController.swift
@@ -5,20 +5,59 @@
 //  Created by stagiaire on 05/05/2017.
 //  Copyright © 2017 IT-Akademy. All rights reserved.
 //
+// c8ee35350d419acb
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    var location:String = "Lyon"
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://api.wunderground.com/api/c8ee35350d419acb/conditions/q/FR/\(self.location).json").validate().responseJSON { (response:DataResponse<Any>) in
+            if response.result.isSuccess {
+                
+                if let json = response.result.value as? [String:Any] {
+                    
+                    print("JSON: \(json)")
+                    
+                    let dataResult = json["current_observation"] as? [String:Any]
+                    let display_location = dataResult?["display_location"] as? [String:Any]
+                    let tempC:Int = dataResult?["temp_c"] as! Int!
+                    let weather = dataResult?["weather"] as! String!
+                    
+                    self.cityLabel.text = display_location?["full"] as! String!
+                    self.tempLabel.text = "Il fait : \(String(tempC)) C"
+                    self.weatherLabel.text = weather
+                    /*let city = display_location?["city"]
+                    let country = display_location?["country"]
+                    let latitude = display_location?["latitude"]
+                    
+                    self.infoCity.text = city as! String?
+                    self.infoCountry.text = country as! String?
+                    self.infoLatitude.text = latitude as! String?
+                    */
+                }
+            }
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
